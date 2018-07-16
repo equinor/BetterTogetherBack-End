12b5ac71707e1d22d139ca88f64f0611cce4bf52
ohald <ohald@statoil.com>
2018-07-16 14:14:29 +0200

    Add dummy test

--- tests/UnitTest.py
+++ tests/UnitTest.py
@@ -3,8 +3,8 @@ import unittest
 
 class UnitTest(unittest.TestCase):
 
-    def test_fail(self):
-        self.assertEqual(1, 2)
+    def test_dummy(self):
+        self.assertEqual(1, 1)
 
 
 if __name__ == '__main__':
