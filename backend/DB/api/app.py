c1e15a428cedec1135b49247abcd9eff5ad9a492
Magnleik <magnleik@stud.ntnu.no>
2018-07-17 12:39:51 +0200

    Add Python backend DB (#2)
    Add database, queries, and REST-api

---
+++ backend/DB/api/app.py
@@ -0,0 +1,68 @@
+from flask import render_template, request, url_for
+from werkzeug.utils import redirect
+from backend.DB.api import tables
+from backend.DB.api import *
+
+from backend.DB.api import queries
+
+
+@app.route('/')
+def display_db():
+    return render_template(
+        'display_db.html',
+        users=queries.get_all_users(),
+        pizza_pairs=queries.get_pair_since_last_reward('pizza'),
+        cake_pairs=queries.get_pair_since_last_reward('cake'),
+    )
+
+
+#        pairs=Pair.query.all(),
+#        thresholds=Threshold.query.all(),
+#        rewards=Reward.query.all())
+
+
+@app.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        user = tables.User(request.form['username'], request.form['firstname'], request.form['lastname'])
+        queries.add_user(user)
+        return redirect(url_for('display_db'))
+    return render_template('add_user.html')
+
+
+@app.route('/add_pair', methods=['GET', 'POST'])
+def add_pair():
+    if request.method == 'POST':
+        pair = tables.Pair(request.form['person1'], request.form['person2'])
+        db.session.add(pair)
+        db.session.commit()
+        return redirect(url_for('display_db'))
+    return render_template('add_pair.html')
+
+
+@app.route('/edit_user/<string:username>', methods=['GET', 'POST'])
+def edit_user(username):
+    if request.method == 'POST':
+        user = tables.User(username, request.form['firstname'], request.form['lastname'])
+        queries.update_user(user)
+        return redirect(url_for('display_db'))
+    return render_template('edit_user.html', user=queries.get_user_by_username(username))
+
+
+@app.route('/delete_user/<string:username>', methods=['GET', 'POST'])
+def delete_user(username):
+    queries.disable_user(username)
+    return redirect(url_for('display_db'))
+
+
+@app.route("/add_reward/<string:reward_type>", methods=['GET', 'POST'])
+def add_reward(reward_type):
+    reward = tables.Reward(reward_type)
+    queries.add_reward(reward)
+    return redirect(url_for('display_db'))
+
+
+if __name__ == '__main__':
+    db.create_all()
+    db.init_app(app)
+    app.run(debug=True)
---
+++ backend/DB/api/queries.py
@@ -0,0 +1,135 @@
+from sqlalchemy import func, or_, and_
+from backend.DB.api import db
+from backend.DB.api import tables
+
+
+# Queries for User
+def add_user(user):
+    db.session.add(user)
+    db.session.commit()
+
+
+def get_active_users():
+    return tables.User.query.filter_by(active=True).all()
+
+
+def get_all_users():
+    return tables.User.query.all()
+
+
+def get_user_by_username(username):
+    return tables.User.query.filter_by(username=username).first()
+
+
+def delete_user(username):
+    user = tables.User.query.filter_by(username=username).first()
+    db.session.delete(user)
+    db.session.commit()
+
+
+def disable_user(username):
+    tables.User.query.filter_by(username=username).update(
+        {'firstname': "Unknown", 'lastname': "User", 'active': False})
+    db.session.commit()
+
+
+def update_user(user):
+    tables.User.query.filter_by(username=user.username).update(
+        {'active': user.active, 'firstname': user.firstname, 'lastname': user.lastname})
+    db.session.commit()
+
+
+# Queries for pair
+def add_pair(pair):
+    db.session.add(pair)
+    db.session.commit()
+
+
+def get_pair_history():
+    return tables.Pair.query.all()
+
+
+def get_pair_since_last_reward(reward_type):
+    reward_date = db.session.query(
+        db.func.max(tables.Reward.date)).filter_by(reward_type=reward_type).first()
+    if reward_date[0] is None:
+        return get_pair_history()
+    else:
+        return tables.Pair.query.filter(tables.Pair.date >= reward_date[0]).all()
+
+
+def get_pairs_with_user(username):
+    return db.session.query(tables.Pair).filter(
+        or_(tables.Pair.person1 == username, tables.Pair.person2 == username)).all()
+
+
+def get_pairs_from_date(date):
+    return tables.Pair.query.filter(tables.Pair.date >= date).all()
+
+
+def get_pair(date):
+    return tables.Pair.query.filter_by(date=date).first()
+
+
+def update_pair(pair):
+    tables.Pair.query.filter_by(date=pair.date).update(
+        {'person1': pair.person1, 'person2': pair.person2})
+    db.session.commit()
+
+
+# Queries for reward
+def add_reward(reward):
+    db.session.add(reward)
+    db.session.commit()
+
+
+def get_rewards():
+    return tables.Reward.query.all()
+
+
+def get_unused_rewards_by_type(reward_type):
+    return tables.Reward.query.filter(and_(
+        tables.Reward.reward_type == reward_type, tables.Reward.used_reward == 0)).all()
+
+
+def get_last_reward_type():
+    reward = tables.Reward.query(db.func.max(tables.Reward.date)).first()
+    return reward.reward_type
+
+
+def get_last_reward_date():
+    reward = tables.Reward.query(db.func.max(tables.Reward.date)).first()
+    return reward.date
+
+
+def get_earliest_unused_reward(reward_type):
+    reward = tables.Reward.query.filter(
+        and_(tables.Reward.reward_type == reward_type, tables.Reward.used_reward == 0)).first()
+    return reward
+
+
+def use_reward(reward_type):
+    reward_date = db.session.query(db.func.min(tables.Reward.date)).filter(and_(
+        tables.Reward.reward_type == reward_type, tables.Reward.used_reward == 0)).first()[0]
+    if reward_date is None:
+        return None
+    reward = tables.Reward.query.filter_by(date=reward_date).first()
+    reward.used_reward = True
+    db.session.commit()
+    return reward
+
+
+# Queries for threshold
+def add_threshold(threshold):
+    db.session.add(threshold)
+    db.session.commit()
+
+
+def get_threshold(reward_type):
+    return tables.Threshold.query.filter_by(reward_type=reward_type).first()
+
+
+def update_threshold(threshold):
+    tables.Threshold.query.filter_by(reward_type=threshold.reward_type).update(
+        {'threshold': threshold.threshold})
+    db.session.commit()
