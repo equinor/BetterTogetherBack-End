098fd9b2608cfe0acedf60f0666fea4c15687601
ohald <ohald@statoil.com>
2018-07-26 15:08:52 +0200

    Make token variable and reduce to one query for get_last_pair

--- backend/DB/api/routes.py
+++ backend/DB/api/routes.py
@@ -19,14 +19,15 @@ def format_users(users):
 
 @app.route('/status/data')
 def status_data():
+    last_pair = queries.get_pair_history()[-1]
     return jsonify({"cake_count": len(queries.get_pair_since_last_reward("cake")),
                      "pizza_count": len(queries.get_pair_since_last_reward("pizza")),
                      "pizza_thres": queries.get_threshold("pizza").threshold,
                      "cake_thres": queries.get_threshold("cake").threshold,
                      "unused_cake": queries.get_unused_rewards_count_by_type("cake"),
                      "unused_pizza": queries.get_unused_rewards_count_by_type("pizza"),
-                     "last_pair": [queries.get_pair_history()[-1].person1,
-                                   queries.get_pair_history()[-1].person2]})
+                     "last_pair": [last_pair.person1,
+                                   last_pair.person2]})
 
 
 @app.route('/api/user/add', methods=['POST'])
--- backend/DB/api/static/graph_display.js
+++ backend/DB/api/static/graph_display.js
@@ -2,11 +2,13 @@ let width = 1500, height = 750;
 
 let radius = width / 50;
 
-d3.json("api/user/all?token=TEST", (users) => {
+let token ="TEST";
 
-    d3.json("api/pair/count_pair?token=TEST", (edge_users) => {
+d3.json("api/user/all?token="+token, (users) => {
 
-        d3.json("status/data?token=TEST", (status) => {
+    d3.json("api/pair/count_pair?token="+token, (edge_users) => {
+
+        d3.json("status/data?token="+token, (status) => {
 
 
             let user_indices = {};
@@ -51,9 +53,9 @@ d3.json("api/user/all?token=TEST", (users) => {
                 .attr('preserveAspectRatio', 'none')
                 .attr('href', (d) => {
                     if (d.image === "unknown") {
-                        return "../static/images/default.png/?token=TEST"
+                        return "../static/images/default.png/?token="+token
                     } else {
-                        return "../static/images/"+ d.username + ".png/?token=TEST"
+                        return "../static/images/"+ d.username + ".png/?token="+token
                     }
                 });
 
