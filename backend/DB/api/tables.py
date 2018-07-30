a947450735a5528777eaadb11c5868ed577eb305
ohald <ohald@statoil.com>
2018-07-30 08:53:52 +0200

    Fix pair equality check

--- backend/DB/api/tables.py
+++ backend/DB/api/tables.py
@@ -38,10 +38,8 @@ class Pair(db.Model):
     user2 = relationship("User", cascade="all,delete", foreign_keys=[person2])
 
     def __eq__(self, other):
-        if((self.person1 == other.person1 and self.person2 == other.person2) or
-                (self.person1 == other.person2 and self.person2 == other.person1)):
-            return True
-        return False
+        return ((self.person1 == other.person1 and self.person2 == other.person2) or
+                (self.person1 == other.person2 and self.person2 == other.person1))
 
     def __init__(self, person1, person2, date=None):
         if date is None:
