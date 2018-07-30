23fc65294314b5bde2ac9641cd2b70e524b0bfea
ohald <ohald@statoil.com>
2018-07-30 08:50:59 +0200

    Change to unix timestamp

--- backend/DB/api/tables.py
+++ backend/DB/api/tables.py
@@ -1,6 +1,7 @@
+import time
+
 from sqlalchemy import ForeignKey
 import math
-from datetime import datetime
 
 from sqlalchemy.orm import relationship
 
@@ -44,7 +45,9 @@ class Pair(db.Model):
 
     def __init__(self, person1, person2, date=None):
         if date is None:
-            self.date = math.floor(datetime.now().timestamp() * 1000)
+            # Pair class requires integer date, not float.
+            # Use ms not s to round off to avoid conflicts on primary key
+            self.date = math.floor(time.time() * 1000)
         else:
             self.date = date
         self.person1 = person1
@@ -67,7 +70,9 @@ class Reward(db.Model):
 
     def __init__(self, reward_type, date=None):
         if not date:
-            self.date = math.floor(datetime.now().timestamp() * 1000)
+            # Pair class requires integer date, not float.
+            # Use ms not s to round off to avoid conflicts on primary key
+            self.date = math.floor(time.time() * 1000)
         else:
             self.date = date
         self.reward_type = reward_type
