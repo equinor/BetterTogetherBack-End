a80c2b2c92713ec12e0e98b12745663628156885
magnleik <magnleik@stud.ntnu.no>
2018-08-07 08:56:08 +0200

    Make graph_display follow js language standards

--- backend/DB/api/static/graph_display.js
+++ backend/DB/api/static/graph_display.js
@@ -1,3 +1,7 @@
+let simulation;
+let links;
+let node;
+
 let width = document.getElementById("content").clientWidth;
 let height = document.getElementById("content").clientHeight;
 
@@ -5,105 +9,33 @@ let radius = width / 30;
 
 let token = (new URL(document.location)).searchParams.get("token");
 
-let set_height = d3.select("#content")
+let setHeight = d3.select("#content")
     .attr("height", window.innerHeight);
-
 
 d3.json("api/user/all?token=" + token, (users) => {
 
-    d3.json("api/pair/count_pair?token=" + token, (edge_users) => {
+    d3.json("api/pair/count_pair?token=" + token, (edgeUsers) => {
 
         d3.json("api/reward/progress?token=" + token, (status) => {
-
-
-            let user_indices = {};
-            users.forEach((v, i) => user_indices[v.username] = i);
-            let links = edge_users.map((v) => ({
-                'source': user_indices[v.source],
-                'target': user_indices[v.target],
-                'total': v.total,
-            }));
-
-            let setCakeProgress = d3.select('#cake')
-                .attr('style', "width:" + status.cake_count / status.cake_thres * 100 + "%")
-                .attr("aria-valuemax", status.cake_thres)
-                .text(status.cake_count.toString() + "/" + status.cake_thres.toString());
-
-
-            let setPizzaProgress = d3.select('#pizza')
-                .attr('style', "width:" + status.pizza_count / status.pizza_thres * 100 + "%")
-                .attr("aria-valuemax", status.pizza_thres)
-                .text(status.pizza_count.toString() + "/" + status.pizza_thres.toString());
-
-            let setClaimable = d3. select('#status_bar')
-                .append("div")
-                .text("Claimable cake: "+ status['unused_cake'] +
-                    " Claimable pizza: "+ status['unused_pizza']);
-
-            //Add patterns to images
-            let defs = d3.select('#patterns_svg')
-                .selectAll('pattern')
-                .data(users)
-                .enter()
-                .append('pattern')
-                .attr('id', (d) => d.username)
-                .attr('height', "100%")
-                .attr('width', "100%")
-                .attr('patternContentUnits', 'objectBoundingBox')
-                .append('image')
-                .attr('height', 1)
-                .attr('width', 1)
-                .attr('preserveAspectRatio', 'none')
-                .attr('href', (d) => {
-                    if (d.image === "unknown") {
-                        return "../static/images/default.png/?token=" + token;
-                    } else {
-                        return "../static/images/" + d.username + ".png/?token=" + token;
-                    }
-                });
-
-
-            //Add nodes to <class>-es
-            let node = d3.select('#circles')
-                .selectAll('circle')
-                .data(users)
-                .enter().append('circle')
-                .attr('class', 'user_node')
-                .attr('r', radius)
-                .attr('fill', (d) => ('url(#' + d.username + ')'));
-
-
-            let simulation = d3.forceSimulation(users)
-                .force('collision', d3.forceCollide().radius(radius + 10))
-                .force('center', d3.forceCenter(width / 2, height / 2))
-                .force('charge', d3.forceManyBody().strength(10))
-                .force('link', d3.forceLink().links(links).strength(1))
-                //.velocityDecay(0.01)
-                .on('tick', ticked);
-
-
-            let drag_handler = d3.drag()
-                .on("start", drag_start)
-                .on("drag", drag_drag)
-                .on("end", drag_end);
-
-            drag_handler(node);
-
             //drag handler
             //d is the node
-            function drag_start(d) {
-                if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+            function dragStart(d) {
+                if (!d3.event.active) {
+                    simulation.alphaTarget(0.3).restart();
+                }
                 d.fx = d.x;
                 d.fy = d.y;
             }
 
-            function drag_drag(d) {
+            function drag(d) {
                 d.fx = d3.event.x;
                 d.fy = d3.event.y;
             }
 
-            function drag_end(d) {
-                if (!d3.event.active) simulation.alphaTarget(0);
+            function dragEnd(d) {
+                if (!d3.event.active) {
+                    simulation.alphaTarget(0);
+                }
                 d.fx = null;
                 d.fy = null;
             }
@@ -115,38 +47,54 @@ d3.json("api/user/all?token=" + token, (users) => {
             }
 
             function updateLinks() {
-                let class_link = d3.select('#links')
-                    .selectAll('line')
+                let classLink = d3.select("#links")
+                    .selectAll("line")
                     .data(links);
-
-                class_link.enter()
-                    .append('line')
-                    .merge(class_link)
-                    .attr('stroke-width', (d) => d.total)
-                    .attr('stroke', (d) => {
+                classLink.enter()
+                    .append("line")
+                    .merge(classLink)
+                    .attr("stroke-width", (d) => d.total)
+                    .attr("stroke", (d) => {
                         if (isLastPairEdge(d)) {
                             return 'red';
                         } else {
-                            return 'black';
+                            return "black";
                         }
                     })
-                    .attr('x1', (d) => d.source.x)
-                    .attr('y1', (d) => d.source.y)
-                    .attr('x2', (d) => d.target.x)
-                    .attr('y2', (d) => d.target.y);
+                    .attr("x1", (d) => d.source.x)
+                    .attr("y1", (d) => d.source.y)
+                    .attr("x2", (d) => d.target.x)
+                    .attr("y2", (d) => d.target.y);
 
-                class_link.exit().remove()
+                classLink.exit().remove();
+            }
+
+            function handleMouseOver(d, i) {
+                d3.select(this)
+                    .attr("r", 1.2 * radius);
+
+                d3.select("svg").append("text")
+                    .attr("id", "object_selected")
+                    .attr("x", () => d.x + radius * 1.3)
+                    .attr("y", () => d.y - height / 35)
+                    .text(() => d.name);
+            }
+
+            function handleMouseOut(d, i) {
+                d3.select(this)
+                    .attr("r", radius);
+                d3.select("#object_selected").remove();
             }
 
             function updateNodes() {
                 node.enter()
-                    .append('circle')
-                    .attr('r', radius)
+                    .append("circle")
+                    .attr("r", radius)
                     .merge(node)
-                    .attr('cx', (d) => d.x = Math.max(radius, Math.min(width - radius, d.x)))
-                    .attr('cy', (d) => d.y = Math.max(radius, Math.min(height - radius, d.y)))
-                    .on('mouseover', handleMouseOver)
-                    .on('mouseout', handleMouseOut);
+                    .attr("cx", (d) => d.x = Math.max(radius, Math.min(width - radius, d.x)))
+                    .attr("cy", (d) => d.y = Math.max(radius, Math.min(height - radius, d.y)))
+                    .on("mouseover", handleMouseOver)
+                    .on("mouseout", handleMouseOut);
                 node.exit().remove();
             }
 
@@ -175,22 +123,71 @@ d3.json("api/user/all?token=" + token, (users) => {
                 rewardBlink();
             }
 
-            function handleMouseOver(d, i) {
-                d3.select(this)
-                    .attr("r", 1.2 * radius);
+            let userIndices = {};
+            users.forEach((v, i) => userIndices[v.username] = i);
+            links = edgeUsers.map((v) => ({
+                "source": userIndices[v.source],
+                "target": userIndices[v.target],
+                "total": v.total,
+            }));
+            let cakePercent = (status.cake_count / status.cake_thres) * 100;
+            let pizzaPercent = (status.pizza_count / status.pizza_thres) * 100;
 
-                d3.select('svg').append('text')
-                    .attr('id', 'object_selected')
-                    .attr('x', () => d.x + radius * 1.3)
-                    .attr('y', () => d.y - height / 35)
-                    .text(() => d.name);
-            }
+            let setProgressCake = d3.select("#cake-percentage")
+                .attr("style", "width:" + cakePercent + "%")
+                .append("span")
+                .text(status.cake_count + "/" + status.cake_thres);
 
-            function handleMouseOut(d, i) {
-                d3.select(this)
-                    .attr('r', radius);
-                d3.select('#object_selected').remove();
-            }
+            let setProgressPizza = d3.select("#pizza-percentage")
+                .attr("style", "width:" + pizzaPercent + "%")
+                .append("span")
+                .text(status.pizza_count + "/" + status.pizza_thres);
+
+            //Add patterns to images
+            let defs = d3.select("#patterns_svg")
+                .selectAll("pattern")
+                .data(users)
+                .enter()
+                .append("pattern")
+                .attr("id", (d) => d.username)
+                .attr("height", "100%")
+                .attr("width", "100%")
+                .attr("patternContentUnits", "objectBoundingBox")
+                .append("image")
+                .attr("height", 1)
+                .attr("width", 1)
+                .attr("preserveAspectRatio", "none")
+                .attr("href", (d) => {
+                    if (d.image === "unknown") {
+                        return "../static/images/default.png/?token=" + token;
+                    } else {
+                        return "../static/images/" + d.username + ".png/?token=" + token;
+                    }
+                });
+
+            //Add nodes to <class>-es
+            node = d3.select("#circles")
+                .selectAll("circle")
+                .data(users)
+                .enter().append("circle")
+                .attr("class", "user_node")
+                .attr("r", radius)
+                .attr("fill", (d) => ("url(#" + d.username + ")"));
+
+            simulation = d3.forceSimulation(users)
+                .force("collision", d3.forceCollide().radius(radius + 10))
+                .force("center", d3.forceCenter(width / 2, height / 2))
+                .force("charge", d3.forceManyBody().strength(10))
+                .force("link", d3.forceLink().links(links).strength(1))
+                .on("tick", ticked);
+
+
+            let dragHandler = d3.drag()
+                .on("start", dragStart)
+                .on("drag", drag)
+                .on("end", dragEnd);
+
+            dragHandler(node);
         });
     });
 });
